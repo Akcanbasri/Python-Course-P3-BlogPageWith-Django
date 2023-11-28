fb8fc9c0e49c8821595faec42c1a9c6ebeb500cf
Hasan Basri Akcan <akcanhasanbasri@hotmail.com>
2023-11-28 22:17:20 +0300

    django app created

--- blog/settings.py
+++ blog/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = "blog.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'blog',
+		'USER': 'root',
+		'PASSWORD': '1453AsD/',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
 }
+
 
 
 # Password validation
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Istanbul"
 
 USE_I18N = True
 
